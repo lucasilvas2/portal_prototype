8aa3029bfae3494e348664d69bbd931ef9cc0210
lucasilvas2 <lucasoliver1068@gmail.com>
2023-06-25 16:07:29 -0300

    feat: Visualização perfil professor e estudante;

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
-  let path = location.pathname.replace("/", "").replace(".html", "");
-  $(".sidebar-item").removeClass("active");
-  switch (path) {
-    case "home":
-      $("#home-sidebar-btn").addClass("active");
-      break;
-    case "videos":
-      console.log($("#videos-sidebar-btn"));
-      $("#videos-sidebar-btn").addClass("active");
+  let type = localStorage.getItem("type_perfil");
+  console.log(type);
+
+  switch (type) {
+    case "student":
+      $("#profile-name")[0].innerHTML = "Maria Silva";
+      $("#profile-type")[0].innerHTML = "Estudante";
+      $("#profile-avatar")[0].src = "assets/images/faces/3.jpg";
+      $("#profile-salutation")[0].innerHTML = "Olá, Maria!";
       break;
     default:
+      $("#menu-admin")[0].classList.remove("d-none");
       break;
   }
 });
