bc016f315b5cc6e9c368efbb0bdd1a3a19e36fa8
lucasilvas2 <lucasoliver1068@gmail.com>
2023-06-30 13:28:08 -0300

    feat: Criação de usuário; Listagem de comunicados na home e na tabela de noticias;

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,16 +1,28 @@
+const Toast = Swal.mixin({
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener("mouseenter", Swal.stopTimer);
+    toast.addEventListener("mouseleave", Swal.resumeTimer);
+  },
+});
+
 $(document).ready(function () {
   let type = localStorage.getItem("type_perfil");
-  console.log(type);
-
-  switch (type) {
-    case "student":
-      $("#profile-name")[0].innerHTML = "Maria Silva";
-      $("#profile-type")[0].innerHTML = "Estudante";
-      $("#profile-avatar")[0].src = "assets/images/faces/3.jpg";
-      $("#profile-salutation")[0].innerHTML = "Olá, Maria!";
-      break;
-    default:
-      $("#menu-admin")[0].classList.remove("d-none");
-      break;
+  if ($("#menu-admin")[0] != undefined) {
+    switch (type) {
+      case "student":
+        $("#profile-name")[0].innerHTML = "Maria Silva";
+        $("#profile-type")[0].innerHTML = "Estudante";
+        $("#profile-avatar")[0].src = "assets/images/faces/3.jpg";
+        $("#profile-salutation")[0].innerHTML = "Olá, Maria!";
+        break;
+      default:
+        $("#menu-admin")[0].classList.remove("d-none");
+        break;
+    }
   }
 });
